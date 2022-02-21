c2380a0e2c27e8e59de23a081ce1b3f49819eaf5
RomainVerhaeghe <71748107+RomainVerhaeghe@users.noreply.github.com>
2022-02-21 16:22:55 +0100

    21/02 AI début

---
+++ BatteShip/EbaucheBN/Classes/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbaucheBN.Classes
+{
+    internal class AI
+    {
+        public static void ShootRandom(BattleShipGrid grid)
+        {
+            Random Hit = new Random();
+            int HitX;
+            int HitY;
+            do
+            {
+                HitX = Hit.Next(1, GameDesign.GridSizeX);
+                HitY = Hit.Next(1, GameDesign.GridSizeY);
+            } while (grid.getCell(HitX, HitY).HitYet == true);
+            //Console.Out.WriteLine(HitX+""+HitY);
+            
+
+        }
+    }
+}
